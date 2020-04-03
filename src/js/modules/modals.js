02438ed8d2f8cc6d743242425ab86511bc754e8f
Andrii Hodniuk <andrii.hodniuk@outlook.com>
2020-04-03 12:44:13 +0200

    Added function to prevent jumping page when the popup is open

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,39 +1,49 @@
-import closeAllModals from "./closeAllModals";
 const modals = () => {
-
-    function bindModal(triggerElement, modalElement, closeElement, closeClickOverlay = true) {
-        const trigger = document.querySelectorAll(triggerElement),
-            modal = document.querySelector(modalElement),
-            close = document.querySelector(closeElement),
-            windows = document.querySelectorAll('[data-modal]');
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+        const trigger = document.querySelectorAll(triggerSelector),
+            modal = document.querySelector(modalSelector),
+            close = document.querySelector(closeSelector),
+            windows = document.querySelectorAll('[data-modal]'),
+            scroll = calcScroll();
 
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
                 if (e.target) {
                     e.preventDefault();
                 }
-                closeAllModals(windows);
-                modal.style.display = 'block';
-                document.body.style.overflow = 'hidden';
-                // document.body.classList.add('modal-open')
+
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
+                modal.style.display = "block";
+                document.body.style.overflow = "hidden";
+                document.body.style.marginRight = `${scroll}px`;
+                // document.body.classList.add('modal-open');
             });
         });
 
         close.addEventListener('click', () => {
-            closeAllModals(windows);
+            windows.forEach(item => {
+                item.style.display = 'none';
+            });
 
-            modal.style.display = 'none';
-            document.body.style.overflow = '';
-            // document.body.classList.remove('modal-open')
+            modal.style.display = "none";
+            document.body.style.overflow = "";
+            document.body.style.marginRight = `0px`;
+            // document.body.classList.remove('modal-open');
         });
 
         modal.addEventListener('click', (e) => {
             if (e.target === modal && closeClickOverlay) {
-                closeAllModals(windows);
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
 
-                modal.style.display = 'none';
-                document.body.style.overflow = '';
-                // document.body.classList.remove('modal-open')
+                modal.style.display = "none";
+                document.body.style.overflow = "";
+                document.body.style.marginRight = `0px`;
+                // document.body.classList.remove('modal-open');
             }
         });
     }
@@ -41,8 +51,23 @@ const modals = () => {
     function showModalByTime(selector, time) {
         setTimeout(function () {
             document.querySelector(selector).style.display = 'block';
-            document.body.style.overflow = 'hidden';
+            document.body.style.overflow = "hidden";
         }, time);
+    }
+
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
     }
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
